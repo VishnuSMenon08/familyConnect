5f2b5b15d8628618197168f151bf3dc9e1286006
Vishnu Menon <vishnumenon@MacBookAir.vodafone.ultrahub>
2025-12-27 01:18:08 +0000

    resolve index.htnml

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    root : 'familyConnect',
     base : '/familyConnect/',
+    build: {
+        outDir: '../dist',      // <-- build goes to root /dist folder
+        emptyOutDir: true
+    }
 });
